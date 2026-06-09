154360bd218433f481ea3b054f1b3a8c81b8b7f0
srinathkiru <srinathkiru23@gmail.com>
2026-06-09 14:12:23 -0400

    Changed port in app

--- app.py
+++ app.py
@@ -70,5 +70,5 @@ def calculate():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 5050))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port)
