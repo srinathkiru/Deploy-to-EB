a079f7262fdb1929fea947b3f2668c1887e8086f
srinathkiru <srinathkiru23@gmail.com>
2026-06-09 13:38:06 -0400

    Added policy to allow ECR token

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,6 +37,8 @@ resource "aws_iam_role" "eb_service_role" {
   })
 }
 
+
+
 resource "aws_iam_role_policy_attachment" "eb_service_role_health" {
   role       = aws_iam_role.eb_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth"
@@ -61,6 +63,26 @@ resource "aws_iam_role" "eb_instance_role" {
   })
 }
 
+resource "aws_iam_role_policy" "eb_ecr_auth" {
+  name = "${var.app_name}-ecr-auth-policy"
+  role = aws_iam_role.eb_instance_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
 resource "aws_iam_role_policy_attachment" "eb_instance_webtier" {
   role       = aws_iam_role.eb_instance_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
